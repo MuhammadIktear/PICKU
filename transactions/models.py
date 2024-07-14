3f97d9c3879b45522ac89dcb6c77885b0d69e222
Muhammad Iktear <iktear500@gmail.com>
2024-07-14 17:32:26 +0600

    add requirements

--- transactions/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from users.models import UserAccount
-
-class Transaction(models.Model):
-    account = models.ForeignKey(UserAccount, related_name='transactions', on_delete=models.CASCADE)
-    amount = models.DecimalField(decimal_places=2, max_digits=12)
-
-    def __str__(self):
-        return f'{self.amount} deposited to {self.account.user.username}'
