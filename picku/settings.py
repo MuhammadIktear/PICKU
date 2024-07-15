d8875ffeb60dd4c8a9e251f08a4843b5e5603617
Muhammad Iktear <iktear500@gmail.com>
2024-07-15 10:40:55 +0600

    update

--- picku/settings.py
+++ picku/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = 'django-insecure-0ec4b*6$w57r@^h9=%((l1bta09ev@xc(tar8895&a55ag)lzn
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://picku.onrender.com','https://*.127.0.0.1']
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://picku.onrender.com",
+]
 
 
 # Application definition
